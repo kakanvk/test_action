427d7fd26905c8b3292cbd4323c65e83d6056d5c
kakanvk <caoka5265@gmail.com>
2024-04-16 20:17:33 +0700

    new commit

--- server/app.js
+++ server/app.js
@@ -6,11 +6,12 @@ const app = express();
 
 // Define a route
 app.get('/', (req, res) => {
-  res.send('Hello, world!');
+    res.send('Hello, world!');
 });
 
 // Start the server
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+    console.log("Hello world by Github Action!");
+    console.log(`Server is running on port ${port}`);
 });
