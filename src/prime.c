cd45d03d095cb41287319ab974e8189ba8df99ca
kakanvk <caoka5265@gmail.com>
2024-04-16 20:43:20 +0700

    Unit test with C

---
+++ src/prime.c
@@ -0,0 +1,13 @@
+#include "prime.h"
+
+int isPrime(int number) {
+    if (number <= 1) {
+        return 0;
+    }
+    for (int i = 2; i * i <= number; i++) {
+        if (number % i == 0) {
+            return 0;
+        }
+    }
+    return 1;
+}
--- start_server.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-cd server
-npm install
-node app.js
