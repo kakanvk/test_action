582d439432f4952f191baacb40a6bca65a6832ac
kakanvk <caoka5265@gmail.com>
2024-04-16 20:08:04 +0700

    Fisrt commit

---
+++ server/app.js
@@ -0,0 +1,16 @@
+// Import dependencies
+const express = require('express');
+
+// Create an Express application
+const app = express();
+
+// Define a route
+app.get('/', (req, res) => {
+  res.send('Hello, world!');
+});
+
+// Start the server
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
---
+++ start_server.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cd server
+npm install
+node app.js
