aa024b301193f92da4d56d72f8e23f3b7801aa33
kakanvk <caoka5265@gmail.com>
2024-04-17 21:11:52 +0700

    #3

--- autograde.sh
+++ autograde.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Compile the Java program
-javac Main.java
+javac "$PWD/src/Main.java"
 
 # Run the program and capture the output
-output=$(java Main)
+output=$(java -cp "$PWD/src" Main)
 
 # Compare the output with expected output
 if [[ "$output" == "Expected output" ]]; then
