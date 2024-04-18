ebc10ab6d5dc7ae98c9b6bb523687df435635129
kakanvk <caoka5265@gmail.com>
2024-04-18 08:56:56 +0700

    #16

--- test/PrimeCheckerTest.java
+++ test/PrimeCheckerTest.java
@@ -4,16 +4,18 @@ import org.junit.Test;
 public class PrimeCheckerTest {
 
     public static void main(String[] args){
+        testIsPrime();
     }
 
     @Test
-    public void testIsPrime() {
+    public static void testIsPrime() {
         assertTrue(PrimeChecker.isPrime(2));
         assertTrue(PrimeChecker.isPrime(3));
         assertTrue(PrimeChecker.isPrime(5));
         assertTrue(PrimeChecker.isPrime(7));
         assertTrue(PrimeChecker.isPrime(11));
         assertTrue(PrimeChecker.isPrime(13));
+
         assertTrue(PrimeChecker.isPrime(4));
         assertFalse(PrimeChecker.isPrime(6));
         assertFalse(PrimeChecker.isPrime(8));
