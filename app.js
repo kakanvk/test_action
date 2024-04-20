59c43aa4cf86be571c5e65af7ea68f17abcffe17
kakanvk <caoka5265@gmail.com>
2024-04-20 13:25:49 +0700

    123

--- app.js
+++ app.js
@@ -2,14 +2,9 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  // Lấy địa chỉ IP của máy chủ từ request object
-  const serverIp = req.connection.remoteAddress;
-  
-  res.send(`Server IP Address: ${serverIp}`);
+  res.send('Hello World!');
 });
 
-const server = app.listen(3000, '0.0.0.0', () => {
-    const serverIp = server.address().address;
-    console.log(`Server is running on ${serverIp}:${server.address().port}`);
-    console.log(`Server IP Address: ${serverIp}`);
-  });
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
